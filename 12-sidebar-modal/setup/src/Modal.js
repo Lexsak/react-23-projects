346016c9462502cc54dc289173810138361e475d
Lexsak <bartoszpiprek123@gmail.com>
2023-10-28 15:28:07 +0200

    12-sidebar-modal first commit

--- 12-sidebar-modal/setup/src/Modal.js
+++ 12-sidebar-modal/setup/src/Modal.js
@@ -1,7 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { FaTimes } from 'react-icons/fa'
 const Modal = () => {
-  return <h2>Modal</h2>
+  const [isModal, setIsModal] = useState(true)
+  return <div className={`modal-overlay  ${isModal ? "show-modal" : ""}`}>
+    <div className="modal-container">
+      <h3>modal content</h3>
+      <button className='close-modal-btn' onClick={() => setIsModal(false)}><FaTimes /></button>
+    </div>
+  </div>
 }
 
 export default Modal
