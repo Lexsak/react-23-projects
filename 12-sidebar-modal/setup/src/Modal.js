ab68843faca3aebce8ebc22a0d70719e626ee511
Lexsak <bartoszpiprek123@gmail.com>
2023-10-28 17:40:02 +0200

    12-sidebar-modal second commit

--- 12-sidebar-modal/setup/src/Modal.js
+++ 12-sidebar-modal/setup/src/Modal.js
@@ -1,11 +1,15 @@
-import React, { useState } from 'react'
-import { FaTimes } from 'react-icons/fa'
+import React, { useState } from 'react';
+import { FaTimes } from 'react-icons/fa';
+import { useGlobalContext } from './context';
+
 const Modal = () => {
-  const [isModal, setIsModal] = useState(true)
-  return <div className={`modal-overlay  ${isModal ? "show-modal" : ""}`}>
+  const { isModalOpen, closeModal } = useGlobalContext();
+
+
+  return <div className={` ${isModalOpen ? 'modal-overlay show-modal' : 'modal-overlay'}`}>
     <div className="modal-container">
       <h3>modal content</h3>
-      <button className='close-modal-btn' onClick={() => setIsModal(false)}><FaTimes /></button>
+      <button className='close-modal-btn' onClick={closeModal}><FaTimes /></button>
     </div>
   </div>
 }
