ee0ed1497511bb24c6d8b0b3cc8e18f46b7ae94b
Lexsak <bartoszpiprek123@gmail.com>
2023-10-30 12:39:29 +0100

    13-stripe-submenus layout and sidebar commit

--- 13-stripe-submenus/setup/src/Hero.js
+++ 13-stripe-submenus/setup/src/Hero.js
@@ -1,8 +1,28 @@
 import React from 'react'
 import phoneImg from './images/phone.svg'
+import { useGlobalContext } from './context'
 
 const Hero = () => {
-  return <h2>hero component</h2>
+  
+  return <section className='hero'>
+    <div className="hero-center">
+      {/* Hero Info */}
+      <article className='hero-info'>
+        <h1>Lorem ipsum dolor sit amet.</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+          Exercitationem quis pariatur debitis blanditiis? 
+          Earum, maxime. Blanditiis repellendus iusto modi aut?
+        </p>
+        <button className='btn'>Start Now</button>
+      </article>
+
+      {/* Phone Image */}
+      <article className='hero-images'>
+        <img src={phoneImg} alt="phone" className='phone-img'/>
+      </article>
+    </div>
+  </section>
 }
 
 export default Hero
