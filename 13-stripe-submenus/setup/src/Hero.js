36ee7a75684f4284448c4bd87af0a365fe9fc476
Lexsak <bartoszpiprek123@gmail.com>
2023-10-30 15:28:17 +0100

    13-stripe-submenus finall commit

--- 13-stripe-submenus/setup/src/Hero.js
+++ 13-stripe-submenus/setup/src/Hero.js
@@ -3,15 +3,16 @@ import phoneImg from './images/phone.svg'
 import { useGlobalContext } from './context'
 
 const Hero = () => {
-  
-  return <section className='hero'>
+  const { closeSubmenu } = useGlobalContext()
+
+  return <section className='hero' onMouseOver={closeSubmenu}>
     <div className="hero-center">
       {/* Hero Info */}
       <article className='hero-info'>
         <h1>Lorem ipsum dolor sit amet.</h1>
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-          Exercitationem quis pariatur debitis blanditiis? 
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Exercitationem quis pariatur debitis blanditiis?
           Earum, maxime. Blanditiis repellendus iusto modi aut?
         </p>
         <button className='btn'>Start Now</button>
@@ -19,7 +20,7 @@ const Hero = () => {
 
       {/* Phone Image */}
       <article className='hero-images'>
-        <img src={phoneImg} alt="phone" className='phone-img'/>
+        <img src={phoneImg} alt="phone" className='phone-img' />
       </article>
     </div>
   </section>
