fdc8c381c36c50e9052cbcfb900fae8a2bd5b92c
Lexsak <bartoszpiprek123@gmail.com>
2023-10-31 10:52:54 +0100

    14-cart remove, increase, decrease Item

--- 14-cart/setup/src/reducer.js
+++ 14-cart/setup/src/reducer.js
@@ -1,7 +1,34 @@
 const reducer = (state, action) => {
     switch (action.type) {
         case 'CLEAR_CART':
-            return {...state, cart: []};
+            return { ...state, cart: [] };
+        case 'REMOVE':
+            return {
+                ...state,
+                cart: state.cart.filter((cartItem) => cartItem.id !== action.payload)
+            }
+        case 'INCREASE':
+            let tempCart = state.cart.map((cartItem) => {
+                if (cartItem.id === action.payload) {
+                    return {...cartItem, amount: cartItem.amount + 1}
+                }
+                return cartItem
+            });
+            return {
+                ...state,
+                cart: tempCart
+            }
+        case 'DECREASE':
+            let tempCart2 = state.cart.map((cartItem) => {
+                if (cartItem.id === action.payload) {
+                    return { ...cartItem, amount: cartItem.amount - 1 }
+                }
+                return cartItem
+            }).filter((cartItem) => cartItem.amount !== 0)
+            return {
+                ...state,
+                cart: tempCart2
+            }
         default:
             return state;
     }
