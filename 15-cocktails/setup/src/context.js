aa15f2f9d23d6c681fd4bbc22993237399024733
Lexsak <bartoszpiprek123@gmail.com>
2023-11-06 11:32:46 +0100

    Single Cocktail page

--- 15-cocktails/setup/src/context.js
+++ 15-cocktails/setup/src/context.js
@@ -9,7 +9,7 @@ const AppProvider = ({ children }) => {
   const [searchTerm, setSearchTerm] = useState('a');
   const [cocktails, setCocktails] = useState([]);
 
-  const fetchDrinks = async () => {
+  const fetchDrinks = useCallback( async () => {
     setLoading(true)
     try {
       const resposne = await fetch(`${url} ${searchTerm}`);
@@ -35,7 +35,7 @@ const AppProvider = ({ children }) => {
       console.log(error);
       setLoading(false)
     }
-  }
+  }, [searchTerm])
 
   useEffect(() => {
     fetchDrinks()
